4f17a98c87e37d23b97a70ba131b8a72b201f6b0
Nuccino92 <anthnucci@hotmail.com>
2021-12-23 20:39:34 -0500

    server file for node

---
+++ index.js
@@ -0,0 +1,7 @@
+// localhost:8080 - should take users the index.html
+
+// localhost:8080/about - should take users to about.html
+
+// localhost:8080/contact-me - should take users to contact-me.html
+
+// 404.html should display any time the user tries to got o a page not listed above
