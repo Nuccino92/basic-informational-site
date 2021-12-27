1cd0fb139d51f88db9c20475de7f3b14e82b7616
Nuccino92 <anthnucci@hotmail.com>
2021-12-27 17:40:07 -0500

    added comment

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// localhost:8080 - should take users the index.html
-
-// localhost:8080/about - should take users to about.html
-
-// localhost:8080/contact-me - should take users to contact-me.html
-
-// 404.html should display any time the user tries to got o a page not listed above
-
 const http = require("http");
 const fs = require("fs");
 
@@ -61,3 +53,5 @@ server.listen(8080, "localhost", () => {
   //this function fires when we start listening
   console.log("listening for requests on port 8080");
 });
+
+//   to run server, in terminal: nodemon index.js
